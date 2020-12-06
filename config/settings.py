ef5a1c1553aae4df44f968161a2c8ade4849448f
Duane Deane <lilboom@Duanes-Air.attlocal.net>
2020-12-06 14:02:53 -0600

    Heroku config

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'hkx$u!y=o(#$(ezsf9937eox&@kt83i*in9pck%1o%6qcwme20'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
